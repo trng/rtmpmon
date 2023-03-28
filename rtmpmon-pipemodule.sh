df5026c0035f28681bf146e7a01d7ec02ab13870
trng <serhy@ukr.net>
2023-03-28 21:32:13 +0300

    major update
    + log to file added
    + increased precision for more accuracy with graph bars
    * multiple bugfixes

--- rtmpmon-pipemodule.sh
+++ rtmpmon-pipemodule.sh
@@ -2,18 +2,35 @@
 
 YEL='\033[1;33m' # Yellow
 NC='\033[0m' # No Color
-declare -x rows cols koef piece_max kbits_max
+
+precision=100000000
+koef_precision=1000
+declare -x rows cols koef piece_max kbits_max foobar foobarlen pbidx
+
+calc_foobar(){
+ piece_max=$(( (cols-44)*2 ))
+  [[ ${piece_max} -le 0 ]] && piece_max=1
+
+  koef=$((koef_precision*kbits_max/piece_max))
+  #koef=`echo "${kbits_max} ${piece_max}" | awk '{printf "%.7f \n", $1/$2}'`
+
+  koef_tmp=`printf "%.0f" ${koef}`
+  [[ ${koef_tmp} -eq 0 ]] && koef=1000
+
+  pbidx=$((precision*${1}/koef))
+  #pbidx=`echo "${kbits_max} ${koef}" | awk '{printf "%.0f \n", $1/$2}'`
+
+  foobarlen=$((pbidx/2/(precision/koef_precision)))
+  foobar=${pb:0:$foobarlen}
+}
+
 
 update_size(){
   rows=$(tput lines) # get actual lines of term
   cols=$(tput cols)  # get actual columns of term
-  piece_max=$(( (cols-44)*2 ))
-  koef=$((kbits_max/piece_max))
-  [[ ${koef} -eq 0  ]] && koef=1
-  pbidx=$((kbits_max/koef))
-  foobarlen=$((pbidx/2))
-  foobar=${pb:0:$foobarlen}
-  printf "\n\n                            ${YEL}%s %d kbps${NC}\n" ${foobar} $kbits_max
+  onepage=$rows
+  calc_foobar $kbits_max
+  printf "\n${YEL}--TIME--   --BANDWIDTH--   %s %d kbps${NC}\n" ${foobar} $kbits_max
 }
 
 trap update_size WINCH
@@ -34,20 +51,28 @@ prev=0
 curr=0
 
 while [ 1 -gt 0 ]; do
+  if (( onepage <= 1 )); then
+    update_size
+  fi
+  onepage=$((onepage-1))
   read curr
   [[ -z ${ddd} ]] &&  printf "\033\1H\033[1A                                                                                                 \r"
   ddd=` date +"%H:%M:%S"`
+
   bpsv=$(( 8*(curr-prev)/1024 ))
+  #bpsv=`echo "${kbits_max}" | awk '{printf "%.7f \n", $1/$2}'`
+
   kbits_limited=$(( bpsv > kbits_max ? kbits_max : bpsv ))
-  pbidx=$((kbits_limited/koef))
-  foobarlen=$((pbidx/2))
-  foobar=${pb:0:$foobarlen}
-  if [ $((pbidx%2)) -eq 1 ]; then
+  calc_foobar $kbits_limited
+
+  if [ $(((pbidx/(precision/koef_precision))%2)) -eq 1 ]; then
     foobar=$foobar'▌'
   fi
+
   if [ $((kbits_limited - kbits_max)) -eq 0 ]; then
     foobar=$foobar'+++'
   fi
-  printf "%s  %6d kbit/sec   %s\n" $ddd $bpsv ${foobar}
+  printf "%s %6d kbit/sec   %s\n" $ddd $bpsv ${foobar}
+  printf "%s %6d kbit/sec\n" $ddd $bpsv >> ${2}
   prev=$curr
 done
